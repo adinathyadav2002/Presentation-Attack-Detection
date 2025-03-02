f7079430ad486ac243045cc7a6dde6df92c4a46c
Prasaar990 <ashok@prasaar.co>
2025-03-02 22:42:31 +0530

    made working for python

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import joblib
@@ -154,5 +155,7 @@ def predict():
     })
 
 
+# Default to 5000 if no PORT env variable
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
