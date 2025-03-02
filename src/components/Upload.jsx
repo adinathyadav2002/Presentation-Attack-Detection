6fd5e50b035efdcab8b6eeae7eccf1f9f17b3279
Prasaar990 <ashok@prasaar.co>
2025-03-02 22:50:24 +0530

    added requirements.txt

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -35,7 +35,7 @@ function Upload() {
 
     try {
       const response = await axios.post(
-        "http://127.0.0.1:5000/predict",
+        "https://presentation-attack-detection.onrender.com/predict",
         formData,
         { headers: { "Content-Type": "multipart/form-data" } }
       );
